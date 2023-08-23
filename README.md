ed23579e2c16889507cb644e151766b880ae8733
Thomas Neal <thomas.neal@hcl.com>
2023-08-23 15:55:46 -0400

    My first action is ready

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# UCDCreateVersionAction
-Repository to hold new github action that creates a new version of a UCD component
+# GitHub Action: Create new UCD component version
+
+This GitHub Action automates the process of triggering the creation of a new UrbanCode Deploy (UCD) component version based on the provided inputs.
+
+## Inputs
+
+1. `component` (required): The name or ID of the component in UrbanCode Deploy.
+2. `version` (required): The name of the new component version in UrbanCode Deploy.
+3. `description` (optional): Description of the new version.
+4. `type` (optional): Type to use for the new version (incremental or full) - full is the default.
+
+## Example Usage
+
+```yaml
+name: Create UCD Component Version
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Create Component Version
+      uses: tpneal/create-ucd-component-version-javascript-action@v1.0
+      with:
+        component: 'MyComp'
+        name: 'v1'
+        description: 'Version 1 of MyComp'
+        type: 'full'
+```
---
+++ index.js
@@ -0,0 +1,52 @@
+const component = process.env.INPUT_COMPONENT;
+const versionName = process.env.INPUT_VERSIONNAME;
+const description = process.env.INPUT_DESCRIPTION;
+const type = process.env.INPUT_TYPE;
+
+const hostname = process.env.INPUT_HOSTNAME;
+const username = process.env.INPUT_USERNAME;
+const password = process.env.INPUT_PASSWORD;
+const disableSSLVerification = process.env.INPUT_DISABLESSLVERIFICATION === 'true';
+const port = process.env.INPUT_PORT;
+const https = require('https');
+
+import('node-fetch')
+  .then((module) => {
+    const fetch = module.default;
+    const apiUrl = 'https://' + hostname + ':' + port + '/cli/version/request?component=' + component + '&name=' + versionname;
+
+    console.log("Triggering creation of new UCD component version with " + apiUrl);
+
+    const authHeader = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+
+
+    const httpsAgent = new https.Agent({
+      rejectUnauthorized: disableSSLVerification === 'true'
+    });
+
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': authHeader // Include the basic authentication header
+      },
+      agent: httpsAgent
+    })
+      .then(response => response.json())
+      .then(result => {
+        console.log('API response:', result);
+        versionId = result.id;
+        console.log('Component Version ID:', versionId);
+      })
+      .catch(error => {
+        console.error('Unable to create component version in UCD : ', error);
+        throw new Error("Terminating!! ");
+      });
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+
+
+
