18d48465e69bbf4799d75d88fc003264f4f7a959
Thomas Neal <thomas.neal@hcl.com>
2023-08-31 15:24:46 -0400

    Use vercel/ncc

--- dist/index.js
+++ dist/index.js
@@ -258,42 +258,31 @@ __nccwpck_require__.e(/* import() */ 460).then(__nccwpck_require__.t.bind(__nccw
       .catch(error => {
         console.error('Unable to create component version in UCD : ', error);
         throw new Error("Terminating!! ");
+      })
+      .then(() => {
+        // Mark the component version creation/import as 'finished' so any
+        // configured Deployment Triggers will fire.
+        const finishUrl = 'https://' + hostname + ':' + port + '/cli/version/finishedImporting?component=' + component + '&version=' + (versionname.length > 0 ? versionname : currentDateTime);
+
+        console.log("Finishing creation of new UCD component version with " + apiUrl);
+        fetch(finishUrl, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Authorization': authHeader // Include the basic authentication header
+          },
+          agent: httpsAgent
+        })
+        .catch(error => {
+          console.error('Unable to mark component version as finished : ', error);
+          throw new Error("Terminating!! ");
+        });
       });
   })
   .catch((error) => {
     console.error('Error:', error);
   });
 
-// Mark the component version creation/import as 'finished' so any configured Deployment Triggers will fire.
-__nccwpck_require__.e(/* import() */ 460).then(__nccwpck_require__.t.bind(__nccwpck_require__, 460, 23))
-  .then((module) => {
-    const fetch = module.default;
-    const apiUrl = 'https://' + hostname + ':' + port + '/cli/version/finishedImporting?component=' + component + '&version=' + (versionname.length > 0 ? versionname : currentDateTime);
-
-    console.log("Finishing creation of new UCD component version with " + apiUrl);
-
-    const authHeader = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
-
-    const httpsAgent = new https.Agent({
-      rejectUnauthorized: disableSSLVerification === 'true'
-    });
-
-    fetch(apiUrl, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': authHeader // Include the basic authentication header
-      },
-      agent: httpsAgent
-    })
-      .catch(error => {
-        console.error('Unable to mark component version as finished : ', error);
-        throw new Error("Terminating!! ");
-      });
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  });
 
 
 
